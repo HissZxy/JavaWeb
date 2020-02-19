031cae43d664c523b45a1766c77418d60e19cbdb
HissZxy <956527824@qq.com>
2020-02-19 21:43:48 +0800

    java  躲避球 全局变量类

---
+++ Constant1.java
@@ -0,0 +1,13 @@
+package cn.sxt.game;
+
+//用于存储常量
+public class Constant
+{
+	public static final int GAME_WIDTH = 500;
+	public static final int GAME_HEIGHT = 500;
+	public static final int SHELL_NUM = 40;
+	public static final int PLANE_SPEED = 5;
+	public static final int SHELL_SPEED = 3;
+	public static final int SLEEP = 50;
+	public static int LIFE = 1000;
+}
